285fa9dac467a4a15ec13020d0728a06718d7eec
Zaki Nabil <liban1@gmx.com>
2023-10-23 22:13:09 +0200

    created UserServiceImpl

--- src/main/java/com/codingtechniques/customloginapplication/service/UserServiceImpl.java
+++ src/main/java/com/codingtechniques/customloginapplication/service/UserServiceImpl.java
@@ -3,8 +3,15 @@ package com.codingtechniques.customloginapplication.service;
 import com.codingtechniques.customloginapplication.dto.UserDto;
 import com.codingtechniques.customloginapplication.model.User;
 import com.codingtechniques.customloginapplication.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     private UserRepository userRepository;
 
@@ -19,7 +26,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User save(UserDto userDto) {
-        User user = new User(userDto.getUsername(), userDto.getPassword(), userDto.getFullname());
+        User user = new User(userDto.getUsername(), passwordEncoder.encode(userDto.getPassword()), userDto.getFullname());
         return userRepository.save(user);
     }
 }
