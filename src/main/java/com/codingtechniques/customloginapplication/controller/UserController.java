357530992326ec9e5ebbc7e9d343d9ba2136de8f
Zaki Nabil <liban1@gmx.com>
2023-10-23 22:13:30 +0200

    created UserController

--- src/main/java/com/codingtechniques/customloginapplication/controller/UserController.java
+++ src/main/java/com/codingtechniques/customloginapplication/controller/UserController.java
@@ -1,30 +1,49 @@
 package com.codingtechniques.customloginapplication.controller;
 
+import com.codingtechniques.customloginapplication.dto.UserDto;
 import com.codingtechniques.customloginapplication.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
+
 @Controller
 public class UserController {
-
+    @Autowired
+    private UserDetailsService userDetailsService;
     private UserService userService;
-    @GetMapping("/home")
-    public String home(){
-        return "Home";
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @GetMapping("login")
-    public String login(){
+    @GetMapping("/home")
+    public String home(Model model, Principal principal){
+        UserDetails userDetails = userDetailsService.loadUserByUsername(principal.getName());
+        model.addAttribute("userdetail", userDetails);
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model, UserDto userDto){
+        model.addAttribute("user", userDto);
         return "login";
     }
 
     @GetMapping("/register")
-    public String register(){
+    public String register(Model model, UserDto userDto){
+        model.addAttribute("user", userDto);
         return "register";
     }
 
     @PostMapping("/register")
-    public String registerSave(){
-        return "register";
+    public String registerSave(@ModelAttribute("user") UserDto userDto){
+        userService.save(userDto);
+        return "redirect:/register?success";
     }
 }
